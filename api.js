kylekatarnls/vicopo

commit 260a3b0977c8caaa99c625225fec91d6ad918513

FRemove useless crawling of buttons and checkboxs

api.js
@@ -1,12 +1,10 @@
 /**
  * Vicopo
  * @author https://github.com/kylekatarnls
- * http://vicopo.selfbuild.fr
+ * https://vicopo.selfbuild.fr
  */
 jQuery(function ($) {
-	var _host = ~(location.protocol + '').indexOf('s')
-		? 'https://www.selfbuild.fr/vicopo'
-		: 'http://vicopo.selfbuild.fr';
+	var _host = location.protocol + '//vicopo.selfbuild.fr';
 	var _cache = {};
 	var _sort = function (a, b) {
 		return a.city - b.city;
@@ -106,9 +104,9 @@ jQuery(function ($) {
 		if($target.data('vicopo-value') !== _input) {
 			var _$targets = $target.data('vicopo-value', _input)
 			.vicopoTargets().each(function () {
-				var $repeater = $(this).hide().vicopoClean();
+				$(this).hide().vicopoClean();
 			});
-			if(_input.length) {
+			if(_$targets.length && _input.length) {
 				$.vicopo(_input, function (_check, _cities) {
 					if(_check === _input) {
 						_$targets.each(function () {

commit d90915e1f9ff321aaf2b75933ce0700adc278397

Fix #3 protocole regression
Fix regression introduced by 260a3b0977c8caaa99c625225fec91d6ad918513

api.js
@@ -4,7 +4,7 @@
  * https://vicopo.selfbuild.fr
  */
 jQuery(function ($) {
-	var _host = location.protocol + '//vicopo.selfbuild.fr';
+	var _host = (location.protocol === 'http:' ? 'http:' : 'https:') + '//vicopo.selfbuild.fr';
 	var _cache = {};
 	var _sort = function (a, b) {
 		return a.city - b.city;