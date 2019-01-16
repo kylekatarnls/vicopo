c0d851c716c9f67a948dd2ea338e881145be7daf
KyleKatarn <kylekatarnls@gmail.com>
2019-01-16 11:04:15 +0100

    Synchronize version 1.2.1

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r") as fh:
 
 setup(
     name = "Vicopo",
-    version = "1.1.9",
+    version = "1.2.1",
     description = "Vicopo est un moyen léger et rapide rechercher une ville française et implémenter des propositions à la volée, l'autocomplétion d'un champ de ville ou de code postal et la conversion de l'un vers l'autre.",
     long_description = long_description,
     long_description_content_type = "text/markdown",
     author = "kylekatarnls",
-    url = "https://github.com/kylekatarnls/vicopo",
+    url = "https://vicopo.selfbuild.fr",
     packages = find_packages(),
 	scripts = ['Vicopo/Vicopo.py'],
 )
--- vicopo.gemspec
+++ vicopo.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'vicopo'
-  s.version     = '1.1.9'
+  s.version     = '1.2.1'
   s.date        = '2019-01-16'
   s.summary     = "API HTTP et Script pour trouver les villes à partir d'un code postal et code postaux à partir d'une ville."
   s.description = "Vicopo est un moyen léger et rapide rechercher une ville française et implémenter des propositions à la volée, l'autocomplétion d'un champ de ville ou de code postal et la conversion de l'un vers l'autre."
