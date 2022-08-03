1d5f318a33a2690b6025580ee6e133822a07d518
KyleKatarn <kylekatarnls@gmail.com>
2022-08-03 21:41:12 +0200

    1.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = "Vicopo",
-    version = "1.2.1",
+    version = "1.3.0",
     description = "Vicopo est un moyen léger et rapide rechercher une ville française et implémenter des propositions à la volée, l'autocomplétion d'un champ de ville ou de code postal et la conversion de l'un vers l'autre.",
     long_description = long_description,
     long_description_content_type = "text/markdown",
